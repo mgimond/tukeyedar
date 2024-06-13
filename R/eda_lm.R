dfddfe8be2f2f23f7e83bc236884411f35007216
Manny Gimond <mgimond@colby.edu>
2024-06-13 10:18:00 -0400

    added robust option

--- R/eda_lm.R
+++ R/eda_lm.R
@@ -1,7 +1,7 @@
 #' @export
-#' @import grDevices
+#' @import grDevices, MASS
 #' @importFrom utils modifyList
-#' @title Least Squares regression plot (with optional LOESS fit)
+#' @title Regression plot (with optional LOESS fit)
 #'
 #' @description \code{eda_lm} generates a scatter plot with a fitted regression
 #'   line. A loess line can  also be added to the plot for model comparison. The
@@ -20,6 +20,7 @@
 #' @param reg Boolean indicating whether a least squares regression line should
 #'   be plotted.
 #' @param poly Polynomial order.
+#' @param robust Boolean indicating if robust regression should be used.
 #' @param w Weight to pass to regression model.
 #' @param sd Boolean determining if standard deviation lines should be plotted.
 #' @param mean.l Boolean determining if the x and y mean lines should be added
@@ -50,28 +51,37 @@
 #' @param ylab Y label for output plot.
 #' @param ... Not used.
 #'
-#' @details The function will plot an OLS regression line and, if requested, a
-#'   loess fit. The plot will also display the +/- 1 standard deviations as
-#'   dashed lines. In theory, if both x and y values follow a perfectly Normal
-#'   distribution, roughly 68 percent of the points should fall in between these
-#'   lines.
+#' @details The function will plot a regression line and, if requested, a loess
+#'   fit. The function adopts the least squares fitting technique by default. It
+#'   defaults to a first order polynomial fit. The polynomial order can be
+#'   specified via the \code{poly} argument.
+#'   \cr\cr
+#'   The plot displays the +/- 1 standard deviations as dashed lines. In
+#'   theory, if both x and y values follow a perfectly Normal distribution,
+#'   roughly 68 percent of the points should fall in between these lines.
 #'   \cr \cr
-#'   The true 68 percent of values can be displayed as grey rectangles by setting
-#'    \code{q=TRUE}. This uses the \code{quantile} function to compute the upper
-#'    and lower bounds defining  the inner 68 percent of values. If the data
-#'    follow a Normal distribution, the grey rectangle edges should coincide
-#'    with the +/- 1SD dashed lines.\cr
-#'    If you wish to show the interquartlie ranges (IQR) instead of the inner
-#'    68 percent of values, simply set \code{q.val = c(0.25,0.75)}.
+#'   The true 68 percent of values can be displayed as grey rectangles by
+#'   setting \code{q=TRUE}. It uses the \code{quantile} function to compute
+#'   the upper and lower bounds defining the inner 68 percent of values. If the
+#'   data follow a Normal distribution, the grey rectangle edges should coincide
+#'   with the +/- 1SD dashed lines.\cr
+#'   If you wish to show the interquartile ranges (IQR) instead of the inner
+#'   68 percent of values, simply set \code{q.val = c(0.25,0.75)}.
 #'   \cr \cr
 #'   The plot has the option to re-express the values via the \code{px} and
 #'   \code{py} arguments. But note that if the re-expression produces \code{NaN}
-#'   values (such as if a negative value is logged) those points will be removed
-#'   from the plot. This will result in fewer observations being plotted. If
-#'   observations are removed as result of a re-expression a warning message
-#'   will be displayed in the console. \cr
-#'   The re-expression powers are shown in the upper right side of the plot.
-#'   To suppress the display of the re-expressions set \code{show.par = FALSE}.
+#'    values (such as if a negative value is logged) those points will be
+#'    removed from the plot. This will result in fewer observations being
+#'    plotted. If observations are removed as result of a re-expression a
+#'    warning message will be displayed in the console.
+#'    \cr
+#'    There-expression powers are shown in the upper right side of the plot. To
+#'   suppress the display of the re-expressions set \code{show.par = FALSE}.
+#'   \cr\cr
+#'   If the \code{robus} boolean argument is set to TRUE, \code{MASS}'s
+#'   built-in robust fitting model, \code{rlm} is used to fit the regression
+#'   line to the data. \code{rlm} arguments can be passed as a list via the
+#'   \code{rlm.d} argument.
 #'
 #' @return Returns residuals, intercept and slope from an OLS fit.
 #'
@@ -107,16 +117,22 @@
 #' eda_lm(df2, x = B20004013, y = B20004007, xlab = "Female", ylab = "Male",
 #'             px = 1/3, py = 0, q = TRUE, loe = TRUE)
 #'
+#' # Fit a second order polynomial
+#' eda_lm(mtcars, hp, mpg, poly = 2)
+#'
+#' # Fit a robust regression model
+#' eda_lm(mtcars, hp, mpg, robust = TRUE)
 
 
 eda_lm <- function(dat, x, y, xlab = NULL, ylab = NULL, px = 1, py = 1,
-                   tukey = FALSE, show.par = TRUE, reg = TRUE, poly = 1, w=NULL,
-                   sd = TRUE, mean.l = TRUE, grey = 0.6, pch = 21,
-                   p.col = "grey50", p.fill = "grey80", size = 0.8, alpha = 0.8,
-                   q = FALSE, q.val = c(0.16,0.84), q.type = 5, loe = FALSE,
-                   lm.col = rgb(1, 0.5, 0.5, 0.8), loe.col = rgb(.3, .3, 1, 1),
-                   stats=FALSE, stat.size = 0.8,
-                   loess.d=list(family = "symmetric", span=0.7, degree=1), ...){
+                   tukey = FALSE, show.par = TRUE, reg = TRUE, poly = 1,
+                   robust = FALSE,  w=NULL, sd = TRUE, mean.l = TRUE,
+                   grey = 0.6, pch = 21, p.col = "grey50", p.fill = "grey80",
+                   size = 0.8, alpha = 0.8, q = FALSE, q.val = c(0.16,0.84),
+                   q.type = 5, loe = FALSE, lm.col = rgb(1, 0.5, 0.5, 0.8),
+                   loe.col = rgb(.3, .3, 1, 1), stats=FALSE, stat.size = 0.8,
+                   loess.d=list(family = "symmetric", span=0.7, degree=1),
+                   rlm.d = list(psi = "psi.bisquare"), ...){
 
   if(is.null(xlab)){
     xlab = as.character(substitute(x))
@@ -158,8 +174,16 @@ eda_lm <- function(dat, x, y, xlab = NULL, ylab = NULL, px = 1, py = 1,
   loess.l  <- modifyList(list(span = 0.5), loess.d)
 
   # Run regression model
-  #M <- lm(y ~ x, weights = w)
-  M <- lm(y ~ poly(x, degree = poly, raw = TRUE), weights = w)
+  if(robust == FALSE) {
+    #M <- lm(y ~ x, weights = w)
+    M <- lm(y ~ poly(x, degree = poly, raw = TRUE), weights = w)
+  } else {
+    rlm.d <- modifyList(list(formula = y ~ poly(x, degree=poly, raw=TRUE),
+                             data = dat), rlm.d)
+    M <- do.call(MASS::rlm, rlm.d)
+  }
+
+
 
   # Generate plots ----
 
@@ -208,7 +232,12 @@ eda_lm <- function(dat, x, y, xlab = NULL, ylab = NULL, px = 1, py = 1,
           srt=0, col="grey70",  cex=0.7)
   }
   title(xlab = xlab, line =1.8, col.lab=plotcol)
-  if(reg == TRUE)  abline(M, lw = 2, col = lm.col )
+  if(reg == TRUE) {
+    #abline(M, lw = 2, col = lm.col )
+    line_x <- with(dat, seq(min(x), max(x), length.out=300))
+    line_y <- predict(M, newdata = data.frame(x = line_x))
+    lines(line_x, line_y, lw = 2, col = lm.col)
+  }
   if (mean.l == TRUE){
     abline(v=mean.x,lty=1,col="grey70")
     abline(h=mean.y, lty=1, col="grey70")
@@ -243,6 +272,8 @@ eda_lm <- function(dat, x, y, xlab = NULL, ylab = NULL, px = 1, py = 1,
 
   # Reset plot parameters
   par(.pardef)
-  print(coef(M))
+  out_coef <- coef(M)
+  names(out_coef) <- c("int", paste0(xlab, "^", 1:poly))
+  print(out_coef)
   invisible(list(residuals = residuals(M), a = coef(M)[1], b = coef(M)[2]))
 }
