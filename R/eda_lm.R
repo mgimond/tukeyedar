d81061c335e5f18d2573100381f7b71f34a510f5
Manny Gimond <mgimond@colby.edu>
2024-07-23 16:04:17 -0400

    modified printed output to reflect poly order

--- R/eda_lm.R
+++ R/eda_lm.R
@@ -89,12 +89,13 @@
 #'   line to the data. \code{rlm} arguments can be passed as a list via the
 #'   \code{rlm.d} argument.
 #'
-#' @return Returns residuals, intercept and slope from an OLS fit.
+#' @return Returns residuals, intercept and coefficient(s) from an OLS
+#'    fit if \code{reg = TRUE}. Returns \code{NULL} otherwise.
 #'
 #' \itemize{
 #'   \item \code{residuals}: Regression model residuals
 #'   \item \code{a}: Intercept
-#'   \item \code{b}: Slope}
+#'   \item \code{b}: Polynomial coefficient(s)}
 #'
 #' @seealso \code{\link[graphics]{plot}} and \code{\link[stats]{loess.smooth}}
 #'   functions
@@ -279,8 +280,14 @@ eda_lm <- function(dat, x, y, xlab = NULL, ylab = NULL, px = 1, py = 1,
 
   # Reset plot parameters
   par(.pardef)
-  out_coef <- coef(M)
-  names(out_coef) <- c("int", paste0(xlab, "^", 1:poly))
-  print(out_coef)
-  invisible(list(residuals = residuals(M), a = coef(M)[1], b = coef(M)[2]))
+
+  # Output residuals and coefficients if regression is set to TRUE
+  if(reg == TRUE) {
+    out_coef <- coef(M)
+    names(out_coef) <- c("int", paste0(xlab, "^", 1:poly))
+    print(out_coef)
+    invisible(list(residuals = residuals(M), a = out_coef[1], b = out_coef[-1]))
+  } else {
+    print(NULL)
+  }
 }
