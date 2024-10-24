239917c7bcc0385ccbc5291536125d3fea0eb100
Manny Gimond <mgimond@colby.edu>
2024-10-24 10:47:43 -0400

    added grid solution

--- R/eda_qqmat.R
+++ R/eda_qqmat.R
@@ -1,6 +1,6 @@
 #' @export
 #' @import grDevices
-#' @importFrom utils modifyList
+#' @importFrom grid utils modifyList
 #' @title Matrix QQ plot
 #'
 #' @description \code{eda_qqmat} Generates a matrix of empirical or Normal QQ plots as well
@@ -80,6 +80,7 @@ eda_qqmat <- function(dat, x, fac, p = 1L, tukey = FALSE, q.type = 5,
                       alpha = 0.8, q = TRUE, b.val = c(0.25,0.75),
                       l.val = c(0.125, 0.875), xlab = NULL, ylab = NULL,
                       title = NULL, t.size = 1.2, ...) {
+  library(grid)
 
   # Check for invalid arguments
   input <- names(list(...))
@@ -125,20 +126,20 @@ eda_qqmat <- function(dat, x, fac, p = 1L, tukey = FALSE, q.type = 5,
     }
   }
 
-  # Get orginal plot state
-  .pardef <- par(no.readonly = TRUE)
+  # # Get orginal plot state
+  # .pardef <- par(no.readonly = TRUE)
 
   # Get lines-to-inches ratio
-  in2line <- ( par("mar") / par("mai") )[2]
+  # in2line <- ( par("mar") / par("mai") )[2]
 
 
   # Create a dummy plot to extract y-axis labels
-  pdf(NULL)
-  plot(x = x, y = x, type = "n", xlab = "", ylab = "", xaxt = "n",
-       yaxt='n', main = NULL)
-
-  y.wid <- max( strwidth( axTicks(2), units="inches")) * in2line + 1.2
-  dev.off()
+  # pdf(NULL)
+  # plot(x = x, y = x, type = "n", xlab = "", ylab = "", xaxt = "n",
+  #      yaxt='n', main = NULL)
+  #
+  # y.wid <- max( strwidth( axTicks(2), units="inches")) * in2line + 1.2
+  # dev.off()
 
   # Loop through each pairs of factors
   lst <- split(x, fac)
@@ -155,24 +156,31 @@ eda_qqmat <- function(dat, x, fac, p = 1L, tukey = FALSE, q.type = 5,
   dim.cm <- lmin / fac_num * 1.3 # plot width and height
   num.plots <- (fac_num^2)   # Number of plots
 
-  layout(matrix(1:num.plots, nrow = fac_num),
-         widths = rep(lcm(dim.cm), num.plots),
-         heights = rep(lcm(dim.cm), num.plots))
+  # layout(matrix(1:num.plots, nrow = fac_num),
+  #        widths = rep(lcm(dim.cm), num.plots),
+  #        heights = rep(lcm(dim.cm), num.plots))
   # Get orginal plot state
 #  .pardef <- par()
 
-  par(pty = "s", col = plotcol, mar = c(0,0,0,0), xpd = FALSE )
-  on.exit(par(.pardef))
+  # par(pty = "s", col = plotcol, mar = c(0,0,0,0), xpd = FALSE )
+  # on.exit(par(.pardef))
 
   # Get axes limits for plots
   if(is.null(xylim)){
-      xylim <- range(unlist(lst))
+      xylim <- range(x)
     }
 
+  grid.newpage()
+  #main <- viewport(width = 0.90, height = 0.90, layout=grid.layout(3, 3, respect = TRUE))
+  main <- viewport(width = unit(1, "npc") - x_margin,
+                   height = unit(1, "npc") - y_margin,
+                   layout=grid.layout(3, 3, respect = TRUE))
+  pushViewport(main)
 
-
-  for(i in as.character(fac_un)){
-    for(j in as.character(fac_un)){
+  for(jj in 1:fac_num){
+    for(ii in 1:fac_num){
+      i <- fac_un[ii]
+      j <- fac_un[jj]
       x <- unlist(lst[i])
       y <- unlist(lst[j])
 
@@ -217,73 +225,113 @@ eda_qqmat <- function(dat, x, fac, p = 1L, tukey = FALSE, q.type = 5,
 
       # Generate plot
 
-      # If factors are identical
-      if(i == j){
-        plot(NULL, type="n", xlab="", ylab="", xlim=xylim, ylim=xylim, asp = 1,
-             yaxt='n', xaxt='n')
-        box(col=plotcol)
-        axis(3,col=plotcol, col.axis=plotcol, padj = 0.0, pos=par("usr")[3],
-             labels = TRUE)
-        axis(2,col=plotcol, col.axis=plotcol, padj = -0.01, pos=par("usr")[4],
-             labels = TRUE, las = 2)
-        next
-      }
+      # Compute margins needed to accomodate labels
+      label <- as.character(max(xylim))
+      label_width <- convertWidth(stringWidth(label), "lines", valueOnly = TRUE)
+      x_margin <- unit(label_width + 0.5, "lines")
+      y_margin <- unit(4, "lines")  # Horizontal margin for x-axis text
 
 
-      # QQ plot: Empirical ----
-      plot( x=x, y=y,  ylab=NA, las=1, yaxt='n', xaxt='n', xlab=NA,
-            col.lab=plotcol, pch = pch, col = p.col, bg = p.fill, cex = size,
-            xlim = xylim, ylim = xylim, asp = 1)
 
-      box(col=plotcol)
+          vp <- viewport(layout.pos.col = ii, layout.pos.row = jj, xscale = xylim, yscale = xylim)
+          pushViewport(vp)
+          grid.rect()
 
-    #  axis(1,col=plotcol, col.axis=plotcol, labels=TRUE, padj = -0.5)
-      axis(1,col=plotcol, col.axis=plotcol, padj = -0.5,
-           labels =  FALSE)
+          if( ii != jj){
+            grid.points(x=unit(x,"native"), y=unit(y,"native"),
+                        gp = gpar(fill = p.fill, col = p.col), pch = pch)
+            grid.lines(gp = gpar(cex = 0.8, col = plotcol))
+          } else {
+            #grid.text("DIAG", gp = gpar(col = "grey"))
+            grid.text(i, gp = gpar(col = "grey"))
+          }
 
-      # axis(2,col=plotcol, col.axis=plotcol, labels=TRUE, las=1, hadj = 0.9,
-      #      tck = -0.02)
-      axis(2,col=plotcol, col.axis=plotcol, las=1, hadj = 0.9, tck = -0.02,
-           labels = if (j == i) TRUE else FALSE)
+          # Add y-axis
+          if( (ii == 1) & (jj %% 2 !=0)) {
+            grid.yaxis(gp = gpar(cex = 0.8, col = plotcol))
+          } else if( (ii == fac_num) & (jj %% 2 ==0)) {
+            grid.yaxis(gp = gpar(cex = 0.8, col = plotcol), main = FALSE)
+          }
 
-   #   mtext(ylab, side=3, adj= -0.06 ,col=plotcol,  padj = -1.2, cex = par("cex"))
-   #   title(xlab = xlab, line =1.8, col.lab=plotcol)
+          # Add x-axis
+          if(jj == 1 & (ii %% 2 == 0)) {
+            grid.xaxis(main = FALSE, gp = gpar(cex = 0.8))
+          } else if (jj == fac_num & (ii %% 2 != 0)){
+            grid.xaxis( gp = gpar(cex = 0.8))
+          }
+          #  grid.text(paste(i,j)) # Used to debug plot placement
 
-      if(!is.null(title)){
-        title(main = title, line =1.2, col.main=plotcol, cex.main=t.size)
-      }
+          upViewport()
 
-      # Add empirical QQ line ----
-      abline(0, 1, col = plotcol)
+
+   #   # If factors are identical
+   #    if(i == j){
+   #      plot(NULL, type="n", xlab="", ylab="", xlim=xylim, ylim=xylim, asp = 1,
+   #           yaxt='n', xaxt='n')
+   #      box(col=plotcol)
+   #      axis(3,col=plotcol, col.axis=plotcol, padj = 0.0, pos=par("usr")[3],
+   #           labels = TRUE)
+   #      axis(2,col=plotcol, col.axis=plotcol, padj = -0.01, pos=par("usr")[4],
+   #           labels = TRUE, las = 2)
+   #      next
+   #    }
+   #
+   #
+   #    # QQ plot: Empirical ----
+   #    plot( x=x, y=y,  ylab=NA, las=1, yaxt='n', xaxt='n', xlab=NA,
+   #          col.lab=plotcol, pch = pch, col = p.col, bg = p.fill, cex = size,
+   #          xlim = xylim, ylim = xylim, asp = 1)
+   #
+   #    box(col=plotcol)
+   #
+   #  #  axis(1,col=plotcol, col.axis=plotcol, labels=TRUE, padj = -0.5)
+   #    axis(1,col=plotcol, col.axis=plotcol, padj = -0.5,
+   #         labels =  FALSE)
+   #
+   #    # axis(2,col=plotcol, col.axis=plotcol, labels=TRUE, las=1, hadj = 0.9,
+   #    #      tck = -0.02)
+   #    axis(2,col=plotcol, col.axis=plotcol, las=1, hadj = 0.9, tck = -0.02,
+   #         labels = if (j == i) TRUE else FALSE)
+   #
+   # #   mtext(ylab, side=3, adj= -0.06 ,col=plotcol,  padj = -1.2, cex = par("cex"))
+   # #   title(xlab = xlab, line =1.8, col.lab=plotcol)
+   #
+   #    if(!is.null(title)){
+   #      title(main = title, line =1.2, col.main=plotcol, cex.main=t.size)
+   #    }
+   #
+   #    # Add empirical QQ line ----
+   #    abline(0, 1, col = plotcol)
 
       # Add medians  ----
-      abline(v = medx, col = "grey80", lty = 2)
-      abline(h = medy, col = "grey80", lty = 2)
-
-      # Add core boxes ----
-      sq <- par("usr") # get plot corners
-      if(q == TRUE){
-        rect(xleft = qx[1], xright = qx[2], ybottom=sq[3],ytop=sq[4],
-             col = rgb(0,0,0,0.05), border = NA)
-        rect(xleft = sq[1], xright = sq[2], ybottom=qy[1],ytop=qy[2],
-             col = rgb(0,0,0,0.05), border = NA)
-        abline(v = lx, lty = 3, col = "grey90")
-        abline(h = ly, lty = 3, col = "grey90")
-      }
+      # abline(v = medx, col = "grey80", lty = 2)
+      # abline(h = medy, col = "grey80", lty = 2)
+      #
+      # # Add core boxes ----
+      # sq <- par("usr") # get plot corners
+      # if(q == TRUE){
+      #   rect(xleft = qx[1], xright = qx[2], ybottom=sq[3],ytop=sq[4],
+      #        col = rgb(0,0,0,0.05), border = NA)
+      #   rect(xleft = sq[1], xright = sq[2], ybottom=qy[1],ytop=qy[2],
+      #        col = rgb(0,0,0,0.05), border = NA)
+      #   abline(v = lx, lty = 3, col = "grey90")
+      #   abline(h = ly, lty = 3, col = "grey90")
+      #}
     }
   }
 
-  # Add power  parameters to plot
-  mtext(side = 3, text=paste0("p=",p), adj=1, cex = 0.65, outer = TRUE)
-
-  # Add axes labels
-  mtext(fac_un, outer = TRUE, side = 1)
-
-  # Reset plot parameters and  output values
-  par(.pardef)
+  # # Add power  parameters to plot
+  # mtext(side = 3, text=paste0("p=",p), adj=1, cex = 0.65, outer = TRUE)
+  #
+  # # Add axes labels
+  # mtext(fac_un, outer = TRUE, side = 1)
+  #
+  # # Reset plot parameters and  output values
+  # par(.pardef)
 
   # data <- data.frame(x, y)
   # names(data) <- c(xlab, ylab)
   # invisible(list(data = data, p = p, fx = fx, fy = fy))
+  popViewport(0)
 }
 
