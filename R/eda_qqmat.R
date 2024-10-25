0fd8274708337758b583680d68d60709a3711ad6
Manny Gimond <mgimond@colby.edu>
2024-10-24 21:14:44 -0400

    testing

--- R/eda_qqmat.R
+++ R/eda_qqmat.R
@@ -1,11 +1,10 @@
 #' @export
 #' @import grDevices
-#' @importFrom grid utils modifyList
+#' @import grid
+#' @importFrom utils modifyList
 #' @title Matrix QQ plot
 #'
-#' @description \code{eda_qqmat} Generates a matrix of empirical or Normal QQ plots as well
-#' as a Tukey mean-difference plots. Can also be used to generate a matrix of symmetry QQ
-#' plots.
+#' @description \code{eda_qqmat} Generates a matrix of empirical QQ plots
 #'
 #' @param dat  Data frame
 #' @param x    Column name assigned to the values
@@ -18,49 +17,40 @@
 #'   algorithms. (See \code{quantile}tile function).
 #' @param diag  Boolean determining if upper diagonal should be plotted
 #' @param xylim X and Y axes limits.
+#' @param resid Boolean determining if residuals should be plotted. Residuals
+#'   are computed using the \code{stat} parameter.
+#' @param stat Statistic to use if residuals are to be computed. Currently
+#'   \code{mean} (default) or \code{median}.
 #' @param plot Boolean determining if plot should be generated.
-#' @param show.par Boolean determining if parameters such as power
-#'   transformation or formula should be displayed.
 #' @param grey Grey level to apply to plot elements (0 to 1 with 1 = black).
 #' @param pch Point symbol type.
 #' @param p.col Color for point symbol.
 #' @param p.fill Point fill color passed to \code{bg} (Only used for \code{pch}
 #'   ranging from 21-25).
-#' @param size Point size (0-1)
+#' @param size Point symbol size (0-1)
 #' @param tic.size Size of tic symbols (defaults to 0.8)
 #' @param alpha Point transparency (0 = transparent, 1 = opaque). Only
 #'   applicable if \code{rgb()} is not used to define point colors.
 #' @param show.med Boolean determining if median lines should be drawn.
 #' @param q Boolean determining if grey quantile boxes should be plotted.
-#' @param b.val Quantiles to define the quantile box parameters. Defaults to the
-#'   IQR. Two values are needed.
-#' @param l.val Quantiles to define the quantile line parameters. Defaults to
-#'   the mid 75\% of values. Two values are needed.
-#' @param xlab X label for output plot. Ignored if \code{x} is a dataframe.
-#' @param ylab Y label for output plot. Ignored if \code{x} is a dataframe.
-#' @param title Title to add to plot.
-#' @param t.size Title size.
+#' @param inner Fraction of mid-values to display in the quantile box. Defaults
+#'   to the IQR.
 #' @param ... Not used
 #'
-#' @details When the function is used to generate an empirical QQ plot, the plot
+#' @details When the function is used to generate an empirical QQ plots, the plot
 #'   will displays the IQR via grey boxes for both x and y values. The box
-#'   widths can be changed via the  \code{b.val} argument. The plot will also
-#'   display the mid 75\% of values via light colored dashed lines. The line
-#'   positions can be changed via the \code{l.val} argument. The middle dashed
-#'   line represents each batch's median value. Console output prints the
-#'   suggested multiplicative and additive offsets. See the QQ plot vignette for
-#'   an introduction on its use and interpretation.\cr
+#'   widths can be changed via the  \code{inner} argument.  The middle dashed
+#'   line represents each batch's median value. \cr
 #'   \cr
 #'
 #' @returns Returns a list with the following components:
 #'
 #' \itemize{
-#'   \item \code{data}: Dataframe with input \code{x} and \code{y} values.
+#'   \item \code{data}: List with input \code{x} and \code{y} values for each
+#'   group.
 #'   May be interpolated to smallest quantile batch.
 #'   Values will reflect power transformation defined in \code{p}.
-#'   \item \code{p}: Re-expression applied to original values.
-#'   \item \code{fx}: Formula applied to x variable.
-#'   \item \code{fy}: Formula applied to y variable.}
+#'   \item \code{p}: Re-expression applied to original values.}
 #'
 #' @references
 #'
@@ -70,20 +60,20 @@
 #'
 #' @examples
 #'
-#' # Passing data as a dataframe
-#'  singer <- lattice::singer
-#'  dat <- singer[singer$voice.part  %in% c("Bass 2", "Tenor 1"), ]
-#'  eda_qq(dat, height, voice.part)
-#'
+#' singer <- lattice::singer
+#' eda_qqmat(singer, height, voice.part)
+#' eda_qqmat(singer, height, voice.part, diag = FALSE)
+#' eda_qqmat(singer, height, voice.part, diag = FALSE, resid = TRUE)
+#' eda_qqmat(mtcars, mpg, cyl,resid = TRUE, q = FALSE)
+
 
 eda_qqmat <- function(dat, x, fac, p = 1L, tukey = FALSE, q.type = 5, diag = TRUE,
                       xylim = NULL, resid = FALSE, stat = mean,
-                      plot = TRUE, show.par = TRUE, grey = 0.6, pch = 21,
-                      p.col = "grey50", p.fill = "grey80", size = 0.7,
-                      tic.size = 0.7,
-                      alpha = 0.8, q = TRUE, show.med = TRUE,  b.val = c(0.25,0.75),
-                      l.val = c(0.125, 0.875), xlab = NULL, ylab = NULL,
-                      title = NULL, t.size = 1.2, ...) {
+                      plot = TRUE, grey = 0.6, pch = 21,
+                      p.col = "grey50", p.fill = "grey80", size = 0.5,
+                      tic.size = 0.7, alpha = 0.8, q = TRUE,
+                      show.med = TRUE, inner = 0.75, ...) {
+
   library(grid)
 
   # Check for invalid arguments
@@ -93,8 +83,6 @@ eda_qqmat <- function(dat, x, fac, p = 1L, tukey = FALSE, q.type = 5, diag = TRU
                                    paste(input[!check], collapse = ", ")))
 
   # Parameters check
-  if (length(b.val)!= 2) stop("The b.val argument must have two values.")
-  if (length(l.val)!= 2) stop("The b.val argument must have two values.")
   if (! as.character(substitute(stat)) %in% c("mean", "median"))
     stop("Stat must be either the mean or the median")
 
@@ -130,6 +118,9 @@ eda_qqmat <- function(dat, x, fac, p = 1L, tukey = FALSE, q.type = 5, diag = TRU
     }
   }
 
+  # Get upper/lower bounds of quantil box
+  b.val = c(.5 - inner / 2 , .5 + inner / 2)
+
   # Loop through each pairs of factors
   lst <- split(x, fac)
   if(resid == TRUE){
@@ -148,17 +139,21 @@ eda_qqmat <- function(dat, x, fac, p = 1L, tukey = FALSE, q.type = 5, diag = TRU
   if(is.null(xylim)){
       xylim <- range(unlist(lst))
       xylim <- c(xylim[1] - diff(xylim) * lim.buffer  , xylim[2] + diff(xylim) * lim.buffer)
-    }
+  }
 
+  # Create empty list (to be used for data output)
+  lstout <- list()
+
+  # Reset plot window
   grid.newpage()
 
-  # Compute margins needed to accomodate labels
+  # Compute margins needed to accommodate labels
   label <- as.character(max(xylim))
   label_width <- convertWidth(stringWidth(label), "lines", valueOnly = TRUE)
   x_margin <- unit(label_width + 0.5, "lines")
   y_margin <- unit(4, "lines")  # Horizontal margin for x-axis text
 
-  #main <- viewport(width = 0.90, height = 0.90, layout=grid.layout(3, 3, respect = TRUE))
+  # Define main viewport
   main <- viewport(width = unit(1, "npc") - x_margin,
                    height = unit(1, "npc") - y_margin,
                    layout=grid.layout(fac_num, fac_num, respect = TRUE))
@@ -185,6 +180,11 @@ eda_qqmat <- function(dat, x, fac, p = 1L, tukey = FALSE, q.type = 5, diag = TRU
         x <- qq$x
         y <- qq$y
 
+        # Save values for output
+        dfout <- data.frame(x,y)
+        row.names(dfout) <- NULL
+        lstout[[paste(i,j)]] <- dfout
+
         # Set point color parameters.
         if(!is.null(alpha)){
           if(p.col %in% colors() & p.fill %in% colors() ){
@@ -193,95 +193,81 @@ eda_qqmat <- function(dat, x, fac, p = 1L, tukey = FALSE, q.type = 5, diag = TRU
           }
         }
 
-        # Generate plots ----
-
-        # Get lines-to-inches ratio
-        # in2line <- ( par("mar") / par("mai") )[2]
-
-        # QQ plot ----
-
         # Get quantile parameters
         qx <- quantile(x, b.val, qtype = q.type)
         qy <- quantile(y, b.val, qtype = q.type)
 
-        lx <- quantile(x, l.val, qtype = q.type)
-        ly <- quantile(y, l.val, qtype = q.type)
-
+        # Compute median values
         medx <- median(x)
         medy <- median(y)
 
-        # Generate plot
+        # Generate plot ----
+        if(plot == TRUE){
+          vp <- viewport(layout.pos.col = ii, layout.pos.row = jj, xscale = xylim, yscale = xylim)
+          pushViewport(vp)
+          grid.rect(gp = gpar(col = plotcol))
 
-        vp <- viewport(layout.pos.col = ii, layout.pos.row = jj, xscale = xylim, yscale = xylim)
-        pushViewport(vp)
-        grid.rect(gp = gpar(col = plotcol))
+          if( ii != jj){
+            grid.points(x=unit(x,"native"), y=unit(y,"native"),
+                        gp = gpar(fill = p.fill, col = p.col,cex = size), pch = pch)
+            grid.lines(gp = gpar(cex = 0.8, col = plotcol))
+          } else {
+            # Print labels on diagonal
+            # Compute factor label size based on label text width
+            vp_side <-convertWidth(unit(1, "npc"), "mm", valueOnly = TRUE)
+            max_width <- vp_side * 0.5  # We use 70% of the available width to fit the text
+            font_size <- max_width / max(nchar(as.character(fac_un)))  # Estimate font size by dividing by text length
 
-        if( ii != jj){
-          grid.points(x=unit(x,"native"), y=unit(y,"native"),
-                      gp = gpar(fill = p.fill, col = p.col,cex = size), pch = pch)
-          grid.lines(gp = gpar(cex = 0.8, col = plotcol))
-        } else {
-          # Print labels on diagonal
-          # Compute factor label size based on label text width
-          vp_side <-convertWidth(unit(1, "npc"), "mm", valueOnly = TRUE)
-          max_width <- vp_side * 0.5  # We use 70% of the available width to fit the text
-          font_size <- max_width / max(nchar(as.character(fac_un)))  # Estimate font size by dividing by text length
+            if(font_size > 2) font_size <- 2
+            grid.text(i, gp = gpar(col = "grey", cex = font_size))
+          }
 
-          if(font_size > 2) font_size <- 2
-          grid.text(i, gp = gpar(col = "grey", cex = font_size))
-        }
+          # Add y-axis
+          if( (ii == 1) & (jj %% 2 !=0)) {
+            grid.yaxis(gp = gpar(cex = tic.size, col = plotcol))
+          } else if( (ii == fac_num) & (jj %% 2 ==0)) {
+            grid.yaxis(gp = gpar(cex = tic.size, col = plotcol), main = FALSE)
+          }
 
-        # Add y-axis
-        if( (ii == 1) & (jj %% 2 !=0)) {
-          grid.yaxis(gp = gpar(cex = tic.size, col = plotcol))
-        } else if( (ii == fac_num) & (jj %% 2 ==0)) {
-          grid.yaxis(gp = gpar(cex = tic.size, col = plotcol), main = FALSE)
-        }
+          # Add x-axis
+          if(jj == 1 & (ii %% 2 == 0)) {
+            grid.xaxis(main = FALSE, gp = gpar(cex = tic.size, col = plotcol))
+          } else if (jj == fac_num & (ii %% 2 != 0)){
+            grid.xaxis( gp = gpar(cex = tic.size, col = plotcol))
+          }
+          #  grid.text(paste(i,j)) # Used to debug plot placement
 
-        # Add x-axis
-        if(jj == 1 & (ii %% 2 == 0)) {
-          grid.xaxis(main = FALSE, gp = gpar(cex = tic.size, col = plotcol))
-        } else if (jj == fac_num & (ii %% 2 != 0)){
-          grid.xaxis( gp = gpar(cex = tic.size, col = plotcol))
-        }
-        #  grid.text(paste(i,j)) # Used to debug plot placement
+          # Add medians
+          if(show.med == TRUE& ii != jj){
+            grid.segments(x0 = medx, x1 = medx, y0 = xylim[1], y1 = xylim[2],
+                          default.units = "native", gp = gpar(col = "grey80", lty = 2))
+            grid.segments(y0 = medy, y1 = medy, x0 = xylim[1], x1 = xylim[2],
+                          default.units = "native", gp = gpar(col = "grey80", lty = 2))
+          }
 
-        # Add medians
-        if(show.med == TRUE& ii != jj){
-          grid.segments(x0 = medx, x1 = medx, y0 = xylim[1], y1 = xylim[2],
-                        default.units = "native", gp = gpar(col = "grey80", lty = 2))
-          grid.segments(y0 = medy, y1 = medy, x0 = xylim[1], x1 = xylim[2],
-                        default.units = "native", gp = gpar(col = "grey80", lty = 2))
-        }
-
-        # Add core boxes ----
-         if(q == TRUE & ii != jj){
-           grid.polygon(x = c(qx[1], qx[2], qx[2], qx[1]),
-                        y = c(xylim[1], xylim[1], xylim[2], xylim[2]),
-                        gp = gpar(fill = rgb(0,0,0,0.05), col = NA),
-                        default.units = "native")
-           grid.polygon(y = c(qy[1], qy[2], qy[2], qy[1]),
-                        x = c(xylim[1], xylim[1], xylim[2], xylim[2]),
-                        gp = gpar(fill = rgb(0,0,0,0.05), col = NA),
-                        default.units = "native")
-        }
-
-
-        upViewport()
+          # Add core boxes ----
+          if(q == TRUE & ii != jj){
+            grid.polygon(x = c(qx[1], qx[2], qx[2], qx[1]),
+                         y = c(xylim[1], xylim[1], xylim[2], xylim[2]),
+                         gp = gpar(fill = rgb(0,0,0,0.05), col = NA),
+                         default.units = "native")
+            grid.polygon(y = c(qy[1], qy[2], qy[2], qy[1]),
+                         x = c(xylim[1], xylim[1], xylim[2], xylim[2]),
+                         gp = gpar(fill = rgb(0,0,0,0.05), col = NA),
+                         default.units = "native")
+          }
+          upViewport()
+        }  # Close plot loop
       }
+    }   # Close ii loop
+  }     # Close jj loop
 
-
-    } # Close ii loop
-  } # Close jj loop
-
-  # # Add power  parameters to plot
-  # mtext(side = 3, text=paste0("p=",p), adj=1, cex = 0.65, outer = TRUE)
+  # Remind user if power parameter was set to value other than 1
   if ( p !=1 )
   message(paste0("Note that a power transformation of ",p," was applied to the data",
           " before they were processed for the plot."))
-
-  invisible(list(data = lst, p = p))
   popViewport(0)
+  invisible(list(qq_values = lstout, p = p))
 }
 
 
